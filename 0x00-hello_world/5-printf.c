d920dad9bc024df1ce6e189b587de166f5c8830e
precious-appiah <appiahprecious99@gmail.com>
2023-04-20 16:56:49 -0700

    printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
+/*this function prints a string and return an  integer */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }

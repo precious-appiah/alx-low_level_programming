001d6cfa972855c2e8d381f37422353cf6b494d8
precious-appiah <appiahprecious99@gmail.com>
2023-04-21 18:48:28 -0700

    8  put char

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,7 +10,7 @@ int main(void)
 	char letter;
 
 	for (i = 0; i <= 10; i++)
-		putchar(( i % 10) + '0');
+		putchar((i % 10) + '0');
 	for (letter = 'a'; i <= 'f'; letter++)
 		putchar(letter);
 	putchar('\n');

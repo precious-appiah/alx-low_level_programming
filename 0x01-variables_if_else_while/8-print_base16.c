c7bb529177cf48e931e8b2765adce6a7f6330857
precious-appiah <appiahprecious99@gmail.com>
2023-04-21 18:36:43 -0700

    put char

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int i;
+	char letter;
+
+	for (i = 0; i <= 10; i++)
+		putchar(( i % 10) + '0');
+	for (letter = 'a'; i <= 'f'; letter++)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}

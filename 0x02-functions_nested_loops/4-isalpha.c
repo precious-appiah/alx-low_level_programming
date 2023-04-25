0daf3a6b69549b075cfeeb53a4931bb0474005de
precious-appiah <appiahprecious99@gmail.com>
2023-04-25 15:20:38 -0700

    letters

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha - checks for letters
+ * @c: The character to be checked
+ * Return: return an integer 1 or zero
+*/
+int _isalpha(int c)
+{
+	if (c <= 97 && c >= 122)
+		return (1);
+	else if (c <= 65 && c >= 90)
+		return (1);
+	else
+		return (0);
+}

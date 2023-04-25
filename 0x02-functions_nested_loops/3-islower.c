0e9c298270724c5bd54bd82124a8e98606f6c5f5
precious-appiah <appiahprecious99@gmail.com>
2023-04-25 14:43:18 -0700

    checks for lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include <ctype.h>
+#include "main.h"
+/**
+ * _islower - code returns 1 for lower
+ * int c - parameter of integer
+ * @c: The character to be checked
+ * Return: return and integer 1 or zero
+*/
+int _islower(int c)
+{
+	if (tolower(c))
+		return (1);
+	else
+		return (0);
+}

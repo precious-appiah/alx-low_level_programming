49e91048168fa551e235181c65f340c571be2c84
precious-appiah <appiahprecious99@gmail.com>
2023-04-25 13:36:04 -0700

    alsphabet inlower case using put char

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,13 @@
 #include "main.h";
+/**
+ * print_alphabet - print lower case alphabets
+ * Return: void
+*/
 void print_alphabet(void)
 {
 	char letter;
-	for(letter = 'a'; letter <= 'z'; letter++)
+
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		putchar(letter);
 	}

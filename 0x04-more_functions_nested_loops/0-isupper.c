94d90ac2ac713e9ddb4dac1a3fc2e576de864bfe
precious-appiah <appiahprecious99@gmail.com>
2023-04-27 09:04:05 -0700

    check for upper cases

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _isupper - checks if alphabet is upper or lower
- * @c --is the parameter passed
+ * @c: is the parameter passed
  * Return: 1 for true and 0 for false
 */
 

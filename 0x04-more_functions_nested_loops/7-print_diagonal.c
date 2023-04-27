196a4586f8fd61b0e384fced0d6edf2481ca3eff
precious-appiah <appiahprecious99@gmail.com>
2023-04-27 11:09:11 -0700

    print \ ntimes using _putchar

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_diagonal - print lines using putchar
+ * @n: takes no parameter
+ * Return: nothing
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		_putchar('\');
+	}
+	_putchar('\n);
+}

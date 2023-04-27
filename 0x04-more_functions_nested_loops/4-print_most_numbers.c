2bf96991c340c964854f4edebd0fdae7733fc627
precious-appiah <appiahprecious99@gmail.com>
2023-04-27 10:01:36 -0700

    print numbers from 0 to 9 using _putchar except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_most_numbers - print 0 to 9 except 2 and 4
+ *@void: takes no paramenter
+ *Return: nothing
+ */
+void print_most_numbers(void)
+{
+	char num;
+
+	for (num = '0'; num <= 9; num++)
+	{
+		if (num != '2' || num != '4')
+			_putchar(num);
+		putchar('\n');
+	}
+}

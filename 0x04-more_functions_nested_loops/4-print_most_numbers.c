84b7de38ca612361697c78a35fd8aa2fcf089a95
precious-appiah <appiahprecious99@gmail.com>
2023-04-27 14:41:20 -0700

    putchar

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,14 +6,13 @@
  */
 void print_most_numbers(void)
 {
-        char num;
-	
+	char num;
+
 	for (num = '0'; num <= '9'; num++)
-        {
-                if (num != '2' ||  num != '4')
+	{
+		if ((num != '2' || num != '4'))
 		{
 			_putchar(num);
 		}
 	}
-        
 }

7fe5d45823b0051a24c5978a6ae7dc1917226206
precious-appiah <appiahprecious99@gmail.com>
2023-05-04 16:57:24 -0700

    compare strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,14 +7,17 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-
-	while ((*(s1 + i) != '\0') && (*(s2 + i) != '\0') && (*(s1 + i) == *(s2 + i)))
-		i++;
-	if (*(s1 + i) < *(s2 + 1))
-		return (-1);
-	else if (*(s1 + i) == *(s2 + 1))
+	while (((*s1 != '\0') && (*s2 != '\0')) && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+	{
 		return (0);
+	}
 	else
-		return (1);
+	{
+		return (*s1 - *s2);
+	}
 }

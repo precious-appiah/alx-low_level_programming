532fbd2e39d8b3582896edcc1d3ad5a12d3157b3
precious-appiah <appiahprecious99@gmail.com>
2023-05-04 15:52:36 -0700

    string copy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ **_strncpy- function to copy
+ * @dest: first param
+ * @src: second param
+ * @n: no ofchars to be copies
+ * Return: char
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while  (i < n && *(src + i) != '\0')
+	{
+		*(dest + i) = *(src + i);
+		i++;
+	}
+	*(dest + 1) = '\0';
+	return (dest);
+}

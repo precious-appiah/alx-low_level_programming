63b6a69499dc727e412deb0b5bb8d2b521dbb42f
precious-appiah <appiahprecious99@gmail.com>
2023-05-04 17:29:37 -0700

    compare strings

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,12 +7,12 @@
  */
 void reverse_array(int *a, int n)
 {
-	int new_array[n];
-	int i = 0;
+	int i, c;
 
-	for (j = n - 2; j <= 0; j++)
+	for (i = 0; (i < (n - 1) / 2); i++)
 	{
-	       new_array[i] = a[j];
-	       i++;
+		c = a[i];
+		a[i] = a[n - 1 - i];
+		a[n - 1 - i] = c;
 	}
 }

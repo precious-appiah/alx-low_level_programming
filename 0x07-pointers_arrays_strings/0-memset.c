6d33d20c98815a8522e09b95c8bdcbf8a31f1e26
precious-appiah <appiahprecious99@gmail.com>
2023-05-08 21:58:51 -0700

    file 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+/**
+ *  _memset - function fill the first @n bytes of the memory area pointed
+ * to by @s with the constant byte @b
+ * @n: bytes of the memory area pointed to by @s
+ *
+ * @s: first param
+ *
+ * @b: second param
+ *
+ * Return: a pointer to the memory area @s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+	s[i] = b;
+	i++;
+	}
+
+	return (s);
+}

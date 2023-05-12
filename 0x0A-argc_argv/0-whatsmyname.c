a28e131250d14800ace85e082b22c5193d02cc36
precious-appiah <appiahprecious99@gmail.com>
2023-05-12 16:02:03 -0700

    prints its name, followed by a new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main - function that prints
+ * @argc: first param
+ * @argv: second param
+ * Return: int
+ */
+int main(int argc, char **argv[0])
+{
+	printf('%s\n', argv[0]);
+	return (0);
+}

dcfd869cb8219da2b12249730c41c246a7201431
precious-appiah <appiahprecious99@gmail.com>
2023-05-12 16:54:00 -0700

    prnt file name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,9 +6,9 @@
  * @argv: second param
  * Return: int
  */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	(void)argc;
+	void(argc);
 
 	printf('%s\n', argv[0]);
 	return (0);

3082b9f370ef63aa989558d5ecd728a1fb6a6cc6
precious-appiah <appiahprecious99@gmail.com>
2023-05-12 17:10:51 -0700

    print count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - function to print count
+ * @argc: arg cunt
+ * @argv: arg vector
+ * Return: int
+ */
+int main(int argc, char **argv)
+{
+	void(argv);
+
+	printf("%d\n", argc);
+	return (0);
+}

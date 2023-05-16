806f673a4214537796476005519c1833527867e2
precious-appiah <appiahprecious99@gmail.com>
2023-05-16 09:52:00 -0700

    that creates an array of chars, and initializes it with a specific char

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,11 +9,18 @@
 char *create_array(unsigned int size, char c)
 {
 	char *char_alloc;
+	int i = 0;
 
 	if (size == 0)
 		return (NULL);
 
 	char_alloc = malloc(sizeof(char) * size);
-	char_alloc[0] = c;
+	if (char_alloc == NULL)
+		return (1);
+	while (i < size)
+	{
+		char_alloc[i] = c;
+		i++;
+	}
 	return (char_alloc);
 }

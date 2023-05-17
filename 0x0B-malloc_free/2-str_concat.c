230efe873abc0ec27f00d37059591ef96f7c6dae
precious-appiah <appiahprecious99@gmail.com>
2023-05-17 09:06:59 -0700

    string concatenation

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,8 +15,10 @@ char *str_concat(char *s1, char *s2)
 	int m, n;
 	int len;
 
-	if ((s1 == NULL) || (s2 == NULL))
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	while (*(s1 + i) != '\0')
 	{
 		i++;

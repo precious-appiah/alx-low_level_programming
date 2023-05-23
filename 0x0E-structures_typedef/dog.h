341d18a1444f7245da5e52e898e1dd333741f66c
precious-appiah <appiahprecious99@gmail.com>
2023-05-23 09:46:07 -0700

    struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG
-#define DOG
+#ifndef DOG_H
+#define DOG_H
 /**
  * struct dog - structure for dog
  * @name: first member
@@ -12,4 +12,12 @@ struct dog
 	float age;
 	char *owner;
 }
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t;
+
+dog_t *new_dog(char *name, float age, char *owner);
+
+void free_dog(dog_t *d);
 #endif

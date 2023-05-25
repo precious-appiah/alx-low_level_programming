c150dfe4e7560b544f9c1214c324e94539948daa
precious-appiah <appiahprecious99@gmail.com>
2023-05-25 13:18:52 -0700

    fixing issue

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <variadic_functions.h>
+/**
+ * print_numbers - function to print all numbers
+ * @separator: separator
+ * @n: first param
+ * Return:void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list arg;
+
+	va_start(arg, n);
+	if (n = NULL)
+		
+	for (i = 0; i < n; i++) 

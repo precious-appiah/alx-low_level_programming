383856720ddca39a09e3a33959c34aa27e7ea190
precious-appiah <appiahprecious99@gmail.com>
2023-05-25 15:20:32 -0700

    function that prints strings

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - function to print strings
+ * @separator: first param
+ * @n: number of params
+ * Return: nthing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list arg;
+
+	va_start(arg, n);
+	if (separator == NULL)
+		separator = "";
+	for (i = 0; i < n; i++)
+	{
+		if (va_arg(arg, char) == NULL)
+			printf("nil");
+		if (i < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(arg);
+}

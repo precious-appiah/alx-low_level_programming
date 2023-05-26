e37986aacd27db69f344d81e3f21b94925678317
precious-appiah <appiahprecious99@gmail.com>
2023-05-25 22:42:28 -0700

    print_All

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -17,16 +17,16 @@ void print_all(const char * const format, ...)
 		{
 			switch (*(format + i))
 			{
-				case "i":
+				case 'i':
 					printf("%d", va_arg(arg, int));
 					break;
-				case "c":
+				case 'c':
 					printf("%c", va_arg(arg, char));
 					break;
-				case "f":
+				case 'f':
 					printf("%f", va_arg(arg, float));
 					break;
-				case "s":
+				case 's':
 					str = va_arg(arg, char*);
 					printf("%s", va_arg(arg, char*));
 					break;

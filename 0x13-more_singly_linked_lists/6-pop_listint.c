25197f7a642052f679dda1ef737b2e2c59f5d209
precious-appiah <appiahprecious99@gmail.com>
2023-06-07 06:58:57 -0700

    function that frees a list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * pop_listint - function that deletes the head node
+ * @head: first parameter
+ * Return:int
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+
+	temp = *head;
+	*head = (*head)->next;
+	free(temp);
+	temp == NULL;
+
+	return ((*head)->n);
+}

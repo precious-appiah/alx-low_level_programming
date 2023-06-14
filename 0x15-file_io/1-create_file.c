6a214ba215a4816cdfdd806ff084a7f8a2ee6f11
precious-appiah <appiahprecious99@gmail.com>
2023-06-14 09:00:04 -0700

    function that opens file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <sys/stat.h>
+
+int fileExists(const char *filename);
+/**
+ * create_file - function to create file
+ * @filename: name of file to be created
+ * @test_content: content to write
+ * Return:int
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	/*return 0 if exist, -1 if it doesn't */
+	int file_exits;
+	FILE *fp = filename;
+	char *storage;
+	mode_t grp_perm, oth_perm = ~S_IWRXG;
+
+	if (filename == NULL)
+		return (-1);
+
+	fp = fopen(filename, "w");
+
+	storage = malloc(strlen(text_content) * sizeof(char));
+
+	if (storage == NULL)
+		return (-1);
+	int count = fwrite(storage, sizeof(char), strlen(text_content), fp);
+
+	if (count != strlen(text_content))
+		return (-1);
+	fclose();
+
+	/*set permissions*/
+	int result = chmod("example.txt", S_IRUSR | S_IWUSR | grp_perm | oth_perm);
+	
+	if (result != 0)
+		return (-1);
+	return (1);
+}
+/*int fileExists(const char *filename)
+{
+	struct stat buffer;
+	return (stat(filename, &buffer) == 0);
+}*/

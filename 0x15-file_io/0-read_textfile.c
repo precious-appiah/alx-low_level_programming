1bad861927a16cd796516323a59627ac47946119
precious-appiah <appiahprecious99@gmail.com>
2023-06-14 05:09:07 -0700

    function that reads a text file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * read_textfile - function to read letters
+ * @filename: location of file
+ * @letters: number of letters to be printed
+ * Return: number of letters printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *fp;
+	char *f_size;
+	ssize_t info_read;
+
+	fp = fopen(filename, "r");
+
+	if (filename == NULL)
+		return (0);
+	if (fp == NULL)
+		return (0);
+
+	f_size = malloc((letters + 1) * sizeof(char));
+
+	if (f_size == NULL)
+		return (0);
+
+	info_read = fread(f_size, sizeof(char), (letters + 1), fp);
+	/**write(1, f_size, (letters + 1));*/
+	return (info_read);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);

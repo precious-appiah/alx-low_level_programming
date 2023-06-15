f8766e26863693fde016f3f9a3fe2aa93707c166
precious-appiah <appiahprecious99@gmail.com>
2023-06-15 08:59:27 -0700

    function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,45 +12,29 @@ int fileExists(const char *filename);
 
 int create_file(const char *filename, char *text_content)
 {
-	/*return 0 if exist, -1 if it doesn't */
-	FILE *fp;
-	char *storage;
-	mode_t grp_perm = ~S_IRWXG;
-	mode_t oth_perm = ~S_IRWXG;
-	size_t count;
-	int result;
+	int fd_open, fd_write;
+	int count = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	fp = fopen(filename, "w");
+	fd_open = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
-	if (text_content[0] == '\0')
+	if (fd_open == -1 || fd_open < 3)
 	{
-		fclose(fp);
-		return (1);
+		close(fd_open);
+		return (-1);
 	}
 
-	storage = malloc(strlen(text_content) * sizeof(char));
-
-	if (storage == NULL)
+	for (count = 0; text_content[count] != '\0'; count++)
+	{
+	}
+	count++;
+	fd_write = write(fd_open, text_content, count);
+	if (fd_write == -1)
+	{
+		close(fd_open);
 		return (-1);
-	count = fwrite(storage, sizeof(char), strlen(text_content), fp);
-
-	if (count != strlen(text_content))
-		return (-1);
-	fclose(fp);
-
-	/*set permissions*/
-	result = chmod("example.txt", S_IRUSR | S_IWUSR | grp_perm | oth_perm);
-	if (result != 0)
-		return (-1);
+	}
 	return (1);
 }
-/*
- * int fileExists(const char *filename)
- * {
- * struct stat buffer;
- * return (stat(filename, &buffer) == 0);
- * }
-*/

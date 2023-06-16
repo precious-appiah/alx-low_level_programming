ddbe66e2926bb78a83e58106e6938a44d9a7b609
precious-appiah <appiahprecious99@gmail.com>
2023-06-16 10:57:15 -0700

    guarding header file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
@@ -9,3 +12,5 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int fileExists(const char *filename);
+
+#endif

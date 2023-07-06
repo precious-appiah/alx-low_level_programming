5d181ea5f93a313e4fee75af703f3ee314042461
precious-appiah <appiahprecious99@gmail.com>
2023-07-06 15:34:09 -0700

    function that converts binary to int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - function to convert binary to decimal
+ * @b: binary number passed to function
+ * Return: unsigned int
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int j;
+	int i;
+
+	if (b == NULL)
+		return (0);
+
+	for (i = 0; i != '\0'; i++)
+	{
+
+		if (b[i] != '1' || b[i] != '0')
+		{
+			return (0);
+		}
+	}
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		j <<= 1;
+		if (b[i] == '1')
+			j += 1;
+	}
+	return (j);
+}

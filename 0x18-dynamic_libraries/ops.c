8bfcbb6e7f15a6347d802887187fa2dde5c1bc12
precious-appiah <appiahprecious99@gmail.com>
2023-11-13 09:04:09 -0800

    calc lib

---
+++ 0x18-dynamic_libraries/ops.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+/**
+ * add- func name
+ * @a: param1
+ * @b:param2
+ * Return: int
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub- func name
+ * @a: param1
+ * @b:param2
+ * Return: int
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul- func name
+ * @a: param1
+ * @b:param2
+ * Return: int
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div- func name
+ * @a: param1
+ * @b:param2
+ * Return: double
+ *
+ */
+double div(int a, int b)
+{
+	return ((double)a / b);
+}
+
+/**
+ * mod- func name
+ * @a: param1
+ * @b:param2
+ * Return: int
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}

aace2f034d2778761e47b4f6419ad374c47483b9
precious-appiah <appiahprecious99@gmail.com>
2023-12-01 15:29:10 -0800

    task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - function to insert node
+ * @ht: hash table
+ * @key: key
+ * @value: value to be inserted
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new_node;
+	unsigned int i;
+
+	if (ht == NULL || key == NULL || *key == NULL)
+		return (0);
+
+	i = key_index(key, ht->size);
+
+	new_node = malloc(sizeof(hash_node_t));
+
+	if (new_node == NULL)
+		return (0);
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+
+	return (1);
+}

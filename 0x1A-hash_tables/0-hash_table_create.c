1973082018ae1deaec10aee81c0136c801eac08e
precious-appiah <appiahprecious99@gmail.com>
2023-12-01 12:15:17 -0800

    task 0- fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,14 +6,14 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table = (hash_table_t *)malloc(sizeof(hash_table_t));
+	hash_table_t *new_table = malloc(sizeof(hash_table_t));
 
 	if (new_table == NULL)
 		return (NULL);
 
 	new_table->size = size;
 
-	new_table->array = (hash_node_t **)malloc(size * sizeof(hash_node_t));
+	new_table->array = malloc(size * sizeof(hash_node_t *));
 
 	if (new_table->array == NULL)
 	{

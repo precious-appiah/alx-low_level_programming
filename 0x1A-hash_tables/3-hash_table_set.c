ba2451deb4641ac6e392490df543c81ddfc86974
precious-appiah <appiahprecious99@gmail.com>
2023-12-01 20:15:58 -0800

    task 1-fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,8 +8,8 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new_node;
-	unsigned int i;
+	hash_node_t *new_node, *current_node;
+	unsigned int i, next_i;
 
 	if (ht == NULL || key == NULL || *key == NULL)
 		return (0);
@@ -21,7 +21,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (new_node == NULL)
 		return (0);
 	new_node->key = strdup(key);
+
 	new_node->value = strdup(value);
+
+	new_node->next = ht->array[i];
+	ht->array[i] = new_node;
 
 	return (1);
 }

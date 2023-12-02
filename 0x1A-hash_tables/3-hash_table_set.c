75c40b5e9537635a1c098f901ad996f8fef02d2c
precious-appiah <appiahprecious99@gmail.com>
2023-12-01 21:05:42 -0800

    3 fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *new_node;
-	unsigned int i;
+	unsigned int i, j;
 
 	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
@@ -20,6 +20,16 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	i = key_index((const unsigned char *)key, ht->size);
+
+	for (j = i; ht->array[j]; j++)
+	{
+		if (strcmp(ht->array[j]->key, key) == 0)
+		{
+			free(ht->array[j]->value);
+			ht->array[j]->value = strdup(value);
+			return (1);
+		}
+	}
 
 	new_node = malloc(sizeof(hash_node_t));
 

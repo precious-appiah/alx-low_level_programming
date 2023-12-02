573e3af063ec52d6183c24968a5299c08b81d282
precious-appiah <appiahprecious99@gmail.com>
2023-12-01 21:52:44 -0800

    task 5 and task 4 betty fix

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -16,7 +16,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	index = key_index((const unsigned char *)key, ht->size);
 
 	if (index > ht->size)
-		return NULL;
+		return (NULL);
 
 	node = ht->array[index];
 
@@ -24,9 +24,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	{
 		if (strcmp(node->key, key) == 0)
 		{
-			return node->value;
+			return (node->value);
 		}
 		node = node->next;
 	}
-	return NULL;
+	return (NULL);
 }
---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - function to print
+ * @ht: table
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned int i;
+
+	if (ht == NULL)
+		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		printf("{%s, %s}", node->key, node->value);
+	}
+}
